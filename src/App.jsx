b0f4758a461a10f4e5ff7cd478202c4656e75ac8
MatheusGesser <matheusmvg@hotmail.com>
2023-03-03 13:53:45 -0300

    feat: add open, close and connect socket functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState, useCallback, useRef } from 'react'
+
+import { ChatClient } from './chat-client';
+
+// Websocket URL
+const URL = '';
 
 export default function App() {
 
@@ -7,6 +12,33 @@ export default function App() {
   const [members, setMembers] = useState([]);
   const [chatRows, setChatRows] = useState([]);
   let name;
+
+  const onSocketOpen = useCallback(() => {
+    name = prompt('Informe o seu nome')?.trim();
+    if (name != null && name != "" && name != undefined) {
+      socket.current?.send(JSON.stringify({ action: 'setName', name }));
+      setIsConnected(true);
+      return
+    }
+    socket.current?.close();
+  }, []);
+
+  const onSocketClose = useCallback(() => {
+    setMembers([]);
+    setIsConnected(false);
+    setChatRows([]);
+  }, []);
+
+  const onConnect = useCallback(() => {
+    if (socket.current?.readyState !== WebSocket.OPEN) {
+      socket.current = new WebSocket(URL);
+      socket.current.addEventListener('open', onSocketOpen);
+      socket.current.addEventListener('close', onSocketClose);
+      socket.current.addEventListener('message', (event) => {
+        onSocketMessage(event.data);
+      });
+    }
+  }, []);
 
   return (
     <>
