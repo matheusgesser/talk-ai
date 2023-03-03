ef57b8f351607df6b1ed13c5618dfcb6917ea765
MatheusGesser <matheusmvg@hotmail.com>
2023-03-03 13:48:53 -0300

    feat: create main component chat-client

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 export default function App() {
 
+  const socket = useRef(WebSocket);
+  const [isConnected, setIsConnected] = useState(false);
+  const [members, setMembers] = useState([]);
+  const [chatRows, setChatRows] = useState([]);
+  let name;
+
   return (
     <>
-      <h1>Hello World</h1>
+      <ChatClient
+        isConnected={isConnected}
+        members={members}
+        chatRows={chatRows}
+      />
     </>
   )
 }
---
+++ src/chat-client.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ChatClient = ({ isConnected, members, chatRows }) => {
+  return (
+    <h1>Hello World</h1>
+  )
+}
