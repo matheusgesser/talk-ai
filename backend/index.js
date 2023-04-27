2e88b3db21175a7c56c48bce4ce964ba344a9be5
MatheusGesser <matheusmvg@hotmail.com>
2023-04-27 09:28:47 -0300

    refactor: improved legibility

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,14 @@
+require('dotenv').config()
 const AWS = require('aws-sdk')
-
 const ENDPOINT = '' // ! WEBSOCKET_KEY
 const client = new AWS.ApiGatewayManagementApi({ endpoint: ENDPOINT })
-const names = {};
-
 const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config()
-
 const configuration = new Configuration({
   apiKey: '', // ! CHATGPT_API_KEY
 });
 const openai = new OpenAIApi(configuration);
 
-async function runCompletion(message) {
-
+async function runCompletion(message) {  
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: message,
@@ -23,19 +18,13 @@ async function runCompletion(message) {
     frequency_penalty: 0,
     presence_penalty: 0,
   });
-
-  console.log(completion.data.choices[0].text)
-
   return completion.data.choices[0].text
 }
 
 const sendToBot = async (ids, mess) => {
-
   let res = await runCompletion(mess);
-
   let message = { publicMessage: `gptidentifier# ${res}` }
   const all = ids.map(i => sendToOne(i, message));
-
   return Promise.all(all);
 };
 
@@ -45,32 +34,30 @@ const sendToOne = async (id, body) => {
       'ConnectionId': id,
       'Data': Buffer.from(JSON.stringify(body))
     }).promise();
-  } catch (e) {
-    console.log(e);
+  } catch (err) {
+    console.error(err);
   }
 };
 
 const sendToAll = async (ids, body) => {
-  console.log("BODY:", body)
   const all = ids.map(i => sendToOne(i, body));
   return Promise.all(all);
 };
 
+const names = {};
 exports.handler = async (event) => {
-
   if (event.requestContext) {
     const connectionId = event.requestContext.connectionId;
     const routeKey = event.requestContext.routeKey;
-
     let body = {};
     try {
       if (event.body) {
         body = JSON.parse(event.body);
       }
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
-
+    
     switch (routeKey) {
       case '$connect':
         break;
@@ -97,7 +84,6 @@ exports.handler = async (event) => {
         const to = Object.keys(names).find(key => names[key] === body.to);
         await sendToOne(to, { privateMessage: `[Privado] ${names[connectionId]}: ${body.message}` });
         break;
-      default:
     }
   }
 
@@ -106,4 +92,5 @@ exports.handler = async (event) => {
     body: JSON.stringify('Lambda a todo vapor!'),
   };
   return response;
+  
 };
